f012bd672905d4e8234e594b23b92ccc7e80fb71
actor <gizatullininsaf25@gmail.com>
2024-12-01 22:17:19 +0300

    day1: part2 done

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,27 @@
+use std::collections::HashMap;
+use std::fs;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let mut data = String::new();
+    let mut smallest_left: Vec<usize> = Vec::new();
+    let mut smallest_right: Vec<usize> = Vec::new();
+    let mut f = fs::File::open("day1/src/input.txt").unwrap();
+    f.read_to_string(&mut data).unwrap();
+    for line in data.split('\n') {
+        let mut parts = line.split_whitespace();
+        if let (Some(a), Some(b)) = (parts.next(), parts.next()) {
+            smallest_left.push(a.parse::<usize>().unwrap());
+            smallest_right.push(b.parse::<usize>().unwrap());
+        }
+    }
+    smallest_left.sort();
+    smallest_right.sort();
+    let mut appears_count: HashMap<usize, usize> = HashMap::new();
+    for i in smallest_left.iter() {
+        let count = smallest_right.iter().filter(|&x| *x == *i).count();
+        appears_count.insert(*i, count);
+    }
+    let result = appears_count.iter().map(|(a,b)| a*b).sum::<usize>();
+    println!("Part: {}", result);
 }
